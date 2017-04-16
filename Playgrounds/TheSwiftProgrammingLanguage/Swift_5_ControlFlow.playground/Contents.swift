7aeae80d1c3e6d4e7313c133af52ffc5011e7dd3
ShannonChenCHN <273879688@qq.com>
2017-04-16 11:56:39 +0800

    Update Swift playgrounds for chapter 5

--- Playgrounds/TheSwiftProgrammingLanguage/Swift_5_ControlFlow.playground/Contents.swift
+++ Playgrounds/TheSwiftProgrammingLanguage/Swift_5_ControlFlow.playground/Contents.swift
@@ -90,11 +90,195 @@ while square < finalSquare {
 print("Game over!")
 
 
+//___________ Repeat-While
+var needExit = false
+repeat {
+    for i in 1..<10 {
+        if i == 8 {
+            needExit = true
+        }
+    }
+    
+} while !needExit
+
+
+
 // ======================= Conditional Statements ==========================
+
+let temperatureInFahrenheit = 72
+
+if temperatureInFahrenheit <= 32 {
+    
+    print("It's very cold.")
+    
+} else if temperatureInFahrenheit >= 86 {
+    
+    print("It's really warm.")
+    
+} else {
+    
+    print("It's not that cold.")
+    
+}
+
+
+//-_____________________ Switch
+
+let anotherCharacter: Character = "a"
+
+// The body of each case must contain at least one executable statement.
+//switch anotherCharacter {
+//case "a":
+//case "A":
+//    print("The letter A")
+//default:
+//    print("Not the letter a")
+//}
+
+switch anotherCharacter {
+    case "a", "A":
+        print("The letter A")
+    default:
+        print("Not the letter a")
+}
+
+// Interval matching
+let approximateCount = 62
+let countedThings = "moons orbiting Saturn"
+var naturalCount: String
+
+switch approximateCount {
+case 0:
+    naturalCount = "no"
+case 1..<5:
+    naturalCount = "a few"
+case 5..<12:
+    naturalCount = "several"
+case 12..<100:
+    naturalCount = "dozens of"
+case 100..<1000:
+    naturalCount = "hundreds of"
+    
+default:
+    naturalCount = "many"
+}
+
+
+
+// Tuples
+let somePoint = (1, 1)
+
+switch somePoint {
+case (0, 0):
+    print("(0, 0) is at the origin")
+case (_, 0):
+    print("(\(somePoint.0), 0) is on the x-axis")
+case (0, _):
+    print("(0, \(somePoint.1)) is on the y-axis")
+case (-2...2, -2...2):
+    print("(\(somePoint.0), \(somePoint.1)) is inside the box")
+default:
+    print("(\(somePoint.0), \(somePoint.1)) is outside of the box")
+}
+
+
+// Value binding
+let anotherPoint = (2, 0)
+switch anotherPoint {
+case (let x, 0):
+    print("on the x-axis with an x vlaue of \(x)")
+case (0, let y):
+    print("on te y-axis with a y value of \(y)")
+case let(x, y):
+    print("comewhere else at (\(x), \(y))")
+}
+
+
+// Where
+let yetAnotherPoint = (1, -1)
+switch yetAnotherPoint {
+case let(x, y) where x == y:
+    print("(\(x), \(y)) is on the line x == y")
+case let (x, y) where x == -y:
+    print("(\(x), \(y)) is on the line x == -y")
+case let (x, y):
+    print("(\(x), \(y)) is just some arbitrary point")
+}
+
+// Compound cases
+let stillAnotherPoint = (9, 0)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("On an axis, \(distance) from the origin")
+default:
+    print("Not on an axis")
+}
 
 
 // ======================= Control Transfer Statements ==========================
 
+// Fallthrough
+let integerToDescribe = 5
+var description = "The number \(integerToDescribe) is"
+switch integerToDescribe {
+case 2, 3, 5, 7, 11, 13, 17, 19:
+    description += " a prime number, and also"
+    fallthrough
+default:
+    description += " an integer."
+}
+
+print(description)
+
+
+// Labeled Statements
+var myVar = 4
+myLoop: while myVar < 10 {
+    
+    myVar += 1
+    
+    switch myVar % 2 {
+    case 0:
+        print("\(myVar) is an even number.")
+        break myLoop
+    case 1:
+        print("\(myVar) is an odd number.")
+        continue myLoop
+    default:
+        break
+    }
+    
+    
+}
+
 // ======================= Early Exit ==========================
 
+
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    
+    print("Hello \(name)!")
+    
+    guard let location = person["location"] else {
+        print("I hope the weather is nice near you.")
+        return
+    }
+    
+    print("I hope the weather is nice in \(location)")
+    
+}
+
+
+
 // ======================= Checking API Availability ==========================
+
+
+if #available(iOS 10, macOS 10.12, *) {
+    // Use iOS 10 APIs on iOS, and use macOS 10.12 APIs on macOS
+} else {
+    // Fall back to earlier iOS and macOS APIs
+}
+
+
