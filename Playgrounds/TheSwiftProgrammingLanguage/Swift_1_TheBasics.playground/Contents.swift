46e75b82a0ade4a12b63c1ca16e91a2a34237060
ShannonChenCHN <273879688@qq.com>
2017-04-16 10:44:09 +0800

    Update Swift playgrounds for chapter 1 and chapter 2

--- Playgrounds/TheSwiftProgrammingLanguage/Swift_1_TheBasics.playground/Contents.swift
+++ Playgrounds/TheSwiftProgrammingLanguage/Swift_1_TheBasics.playground/Contents.swift
@@ -82,6 +82,7 @@ let meaningOfLife = 42
 
 let pi = 3.14159
 // pi is inferred to be of type Double
+// Type inference enables a compiler to deduce the type of a particular expression automatically when it compiles your code, simply by examining the values you provide.
 
 let anotherPi = 3 + 0.14159
 // anotherPi is also inferred to be of type Double
@@ -90,24 +91,202 @@ let anotherPi = 3 + 0.14159
 
 // ======================= Numeric Literals ==========================
 
+// Integer literals
 let decimalInteger = 17
 let binaryInteger = 0b10001
 let octalInteger = 0o21
 let hexadecimalInteger = 0x11
 print("\(decimalInteger), \(binaryInteger), \(octalInteger), \(hexadecimalInteger)")
 
+// Floating-point literals
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+
+
+// Numeric literals can contain extra formatting to make them easier to read.
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
 // ======================= Numeric Type Conversion ==========================
 
+// Integer Conversion
+//let cannotBeNegative: UInt8 = -1  // UInt8 cannot store negative numbers, and so this will report an error
+//let tooBig: Int8 = Int8.max + 1 // Int8 cannot store a number larger than its maximum value
+
+
+// To convert one specific number type to another, you initialize a new number of the desired type with the existing value.
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+
+// Integer and Floating-Point Conversion
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let anotherPi_1 = Double(three) + pointOneFourOneFiveNine
+let integerPi = Int(anotherPi_1)
 
 // ======================= Type Aliases ==========================
 
+// Type aliases define an alternative name for an existing type.
+typealias AudioSample = UInt16
+var maxAmpltitudeFound = AudioSample.min
+
+
 // ======================= Booleans ==========================
 
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    print("Mmm, tasty turnips!")
+} else {
+    print("Eww, turnips ar horrible.")
+}
+
+
+// Swift’s type safety prevents non-Boolean values from being substituted for Bool.
+//let i = 1
+//if i {
+//    
+//}
+
+let i = 1
+if i == 1 {
+    print("This Ok for if-else")
+}
+
 // ======================= Tuples ==========================
+
+// Tuples group multiple values into a single compound value. The values within a tuple can be of any type and do not have to be of the same type as each other.
+// http404Error is of type (Int, String), and equals (404, "Not Found")
+let http404Error = (404, "Not Found")
+
+// You can decompose a tuple’s contents into separate constants or variables, which you then access as usual:”
+let (statusCode, statusMessage) = http404Error
+print("The status code is \(statusCode)")
+
+print("The status message is \(statusMessage)")
+
+// If you only need some of the tuple’s values, ignore parts of the tuple with an underscore (_) when you decompose the tuple
+let (justTheStatusCode, _) = http404Error
+print("The status code is \(justTheStatusCode))")
+
+
+// Alternatively, access the individual element values in a tuple using index numbers starting at zero.
+print("The status code is \(http404Error.0)")
+print("The status message is \(http404Error.1)")
+
+// You can name the individual elements in a tuple when the tuple is defined:
+let http200Status = (statusCode: 200, description: "OK")
+
 
 // ======================= Optionals ==========================
 
 
+let possibleNumber = "123"
+//let convertedNumber = Int.init(possibleNumber)
+let convertedNumber = Int(possibleNumber) // convertedNumber is inferred to be of type "Int?", or "optional Int
+
+// You set an optional variable to a valueless state by assigning it the special value nil
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+// nil cannot be used with nonoptional constants and variables.
+//var serverResponseCode = 404
+//serverResponseCode = nil
+
+// You can use an if statement to find out whether an optional contains a value by comparing the optional against nil.
+if convertedNumber != nil {
+    
+    // Forced unwrapping of the optional's value
+    print("convertedNumer has an integer value of \(convertedNumber!)")  // Once you’re sure that the optional does contain a value, you can access its underlying value by adding an exclamation mark (!) to the end of the optional’s name
+}
+
+
+// Optional binding
+if let actualNumber = Int(possibleNumber) {
+    print("\"\(possibleNumber)\" has an integer value of \(actualNumber)")
+} else {
+    print("\"\(possibleNumber)\" could not be converted to an integer")
+}
+
+// You can include as many optional bindings and Boolean conditions in a single if statement as you need to, separated by commas.
+if let firstNumber = Int("4"), let secondNumber = Int("42"), firstNumber < secondNumber && secondNumber < 100 {
+    print("\"\(firstNumber) < \(secondNumber) < 100")
+}
+
+// The following if statement is equal to the above if statement
+if let firstNumber = Int("2") {
+    if let secondNumber = Int("43") {
+        if firstNumber < secondNumber && secondNumber < 100 {
+            print("\"\(firstNumber) < \(secondNumber) < 100")
+        }
+    }
+}
+
+// Implicitly Unwrapped Optionals
+let possibleString: String? = "An optional string."
+let forcedString: String = possibleString! // requires an exclamation mark
+
+let assumedString: String! = "An implicitly unwrapped optinal string."
+let implicitString: String = assumedString // no need for an exclamation mark
+
 // ======================= Error Handling ==========================
 
+func canThrowAnError() throws {
+    // this function may or may not throw an error
+}
+
+
+do {
+    try canThrowAnError()
+    
+    // no error was thrown
+} catch {
+    // an error was thrown
+}
+
+
+
+enum SandwichError: Error {
+    case outOfCleanDishes
+    case missingIngredients(ingredients: Any)
+}
+
+
+func makeASandwich() throws {
+    // ...
+}
+
+func eatASandwich() {
+    
+}
+
+func washDishes() {
+    
+}
+
+func buyGroceries(ingredients: Any) {
+    
+}
+
+do {
+    try makeASandwich()
+    eatASandwich()
+    
+} catch SandwichError.outOfCleanDishes {
+    washDishes()
+} catch SandwichError.missingIngredients(let ingredients) {
+    buyGroceries(ingredients: ingredients)
+}
+
+
 // ======================= Assertions ==========================
+
+let age = -3
+//assert(age > 0, "A person's age cannot be less than zero")
+
+
--- Playgrounds/TheSwiftProgrammingLanguage/Swift_2_BasicOperators.playground/Contents.swift
+++ Playgrounds/TheSwiftProgrammingLanguage/Swift_2_BasicOperators.playground/Contents.swift
@@ -6,23 +6,92 @@ import UIKit
 
 // ======================= Terminology ==========================
 
+// Unary operator
+let negtiveInteger = -1
+
+// Binary operator
+let positiveInteger = 2 + 3
+
+// Ternary operator
+let ternary = positiveInteger > 0 ? 1 : 0
+
 // ======================= Assignment Operator ==========================
 
+let b = 10
+var a = 5
+a = b
 
-
+// This is not valid, because x =y does not return a value.
+//if a = b {
+//    
+//}
 
 // ======================= Arithmetic Operators ==========================
 
+let combinedString = "Hello" + "world"
 
 // ======================= Compound Assignment Operators ==========================
 
+var num = 1
+num += 2
+
 // ======================= Comparison Operators ==========================
+
+let name = "world"
+if name == "world" {
+    print("name is \(name)")
+}
+
+//  Swift also provides two identity operators (=== and !==), which you use to test whether two object references both refer to the same object instance.
+let object: NSString = "NSString"
+let anotherObject = object
+
+if object === anotherObject {
+    print("object and anotherObject both refer to the same object instance.")
+    
+}
 
 // ================ Ternary Conditional Operator =========================
 
+
+let contentHeight = 40
+let hasHeader = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+
 // ======================= Nil-Coalescing Operator ==========================
+
+let defaultColorName = "red"
+var userDefinedColorName: String? // defaults to nil
+
+// The following two statement are equal
+// userDefinedColorName is nil, so colorNameToUse is set to the default of "red"
+var colorNameToUser = userDefinedColorName ?? defaultColorName
+var colorName = userDefinedColorName != nil ? userDefinedColorName : defaultColorName
+
 
 // ======================= Range Operators ==========================
 
 
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+
+let names = ["Anna", "Alex", "Brian", "Jack"]
+let count = names.count
+for i in 0..<count {
+    print("person \(i + 1) is called \(names[i])")
+}
+
 // ======================= Logical Operators ==========================
+
+let enteredDoorCode = true
+let passedRetinaScan = false
+let hasDoorKey = true
+let knowsOverridePassword = false
+
+if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword {
+    print("Welcome!")
+} else {
+    print("ACCESS DENIED")
+}
