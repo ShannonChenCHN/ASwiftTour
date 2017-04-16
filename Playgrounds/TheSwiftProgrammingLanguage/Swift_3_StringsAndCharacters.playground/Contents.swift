ee2b98bd4cd07cc90bed91a6e23f008103ad2877
ShannonChenCHN <273879688@qq.com>
2017-04-16 17:22:18 +0800

    Update Swift playgrounds for chapter 3 and chapter 4

--- Playgrounds/TheSwiftProgrammingLanguage/Swift_3_StringsAndCharacters.playground/Contents.swift
+++ Playgrounds/TheSwiftProgrammingLanguage/Swift_3_StringsAndCharacters.playground/Contents.swift
@@ -6,31 +6,200 @@ import UIKit
 
 // ======================= String Literals ==========================
 
+let someString = "Some String literal value"
+
 // ======================= Initializing an Empty String ==========================
 
+var emptyString = ""
+var anotherEmptyString = String()
 
-
+if emptyString.isEmpty {
+    print("Nothing to see here.")
+}
 
 // ======================= String mutability ==========================
 
+var variableString = "Horse"
+variableString += " and carriage"
 
-// ======================= Strings Are Value Type ==========================
+let constantString = "Highlander"
+//constantString += " and another Highlander" // This reports a compile-time error
+
+// ======================= Strings Are Value Types ==========================
+
+/*
+    Swift’s String type is a value type. If you create a new String value, that String value is copied when it is passed to a function or method, or when it is assigned to a constant or variable.
+ */
 
 // ======================= Working with Characters ==========================
 
+// You can access the individual Character values for a String by iterating over its characters property with a for-in loop.
+for character in "Dog!🐶".characters {
+    print(character)
+}
+
+//  you can create a stand-alone Character constant or variable from a single-character string literal by providing a Character type annotation.
+let exclamationMark: Character = "!"
+
+//  String values can be constructed by passing an array of Character values as an argument to its initializer.
+let catCharacters: [Character] = ["C", "a", "t", "!", "🐈"]
+let catString = String(catCharacters)
+print(catString)
+
 // ================ Concatenating Strings and Characters =========================
+
+//  String values can be added together (or concatenated) with the addition operator (+) to create a new String value.
+let string1 = "hello"
+let string2 = " there"
+var welcome = string1 + string2
+
+//  You can also append a String value to an existing String variable with the addition assignment operator (+=).
+var instruction = "look over"
+instruction += string2
+
+// Method: append()
+welcome.append(exclamationMark)
 
 // ======================= String Interpolation ==========================
 
+let multiplier = 3
+let message = "\(multiplier) timer 2.5 is \(Double(multiplier) * 2.5)"
+
 // ======================= Unicode ==========================
 
+// Special characters in string literals
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+let dollarsign = "\u{24}"
+let blackHeart = "\u{2665}"
+let sparklingHeart = "\u{1F496}"
+
+// Extended grapheme clusters
+let eAcute: Character = "\u{E9}"
+let combinedEAcute: Character = "\u{65}\u{301}"
+
+let precomposed: Character = "\u{D55C}"
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"
+let enclosedEAcute: Character = "\u{E9}\u{20DD}"
+let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
 
 // ======================= Counting Characters ==========================
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🕊, Dromedary 🐫"
+print("unusualMenagerie has \(unusualMenagerie.characters.count) characters")
+
+var word = "cafe"
+print("the number of characters in \(word) is \(word.characters.count)")
+word += "\u{301}"
+print("the number of characters in \(word) is \(word.characters.count)")
 
 
 // ======================= Accessing and Modifying a String ==========================
+let greeting = "Guten Tag!"
+//greeting[0] // This will cause complie-error - "cannot subscript String with an Int"
+greeting[greeting.startIndex]
+greeting[greeting.index(before: greeting.endIndex)]
+greeting[greeting.index(after: greeting.startIndex)]
+let index = greeting.index(greeting.startIndex, offsetBy: 7)
+greeting[index]
 
+for index in greeting.characters.indices {
+    print("\(greeting[index])", terminator: "")
+}
+
+// Insert
+welcome.insert("!", at: welcome.endIndex)
+welcome.insert(contentsOf: " there".characters, at: welcome.index(before: welcome.endIndex))
+
+// Remove
+welcome.remove(at: welcome.index(before: welcome.endIndex))
+let range = welcome.index(welcome.endIndex, offsetBy: -6)..<welcome.endIndex
+welcome.removeSubrange(range)
 
 // ======================= Comparing Strings ==========================
 
+// String and character equality
+let quotation = "We're a lot alike, you and I"
+let sameQuotation = "We're a lot alike, you and I."
+if quotation == sameQuotation {
+    print("These two strings are considered equal")
+}
+
+//_______________ Two String values (or two Character values) are considered equal if their extended grapheme clusters are canonically equivalent.
+
+// "Voulez-vous un café?" using LATIN SMALL LETTER E WITH ACUTE
+let eAcuteQuestion = "Voulez-vous un caf\u{E9}?"
+
+// "Voulez-vous un café?" using LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
+let combinedEAcuteQuestion = "Voulez-vous un caf\u{65}\u{301}?"
+
+// LATIN SMALL LETTER E WITH ACUTE (U+00E9) is canonically equivalent to LATIN SMALL LETTER E (U+0065) followed by COMBINING ACUTE ACCENT (U+0301). Both of these extended grapheme clusters are valid ways to represent the character é, and so they are considered to be canonically equivalent.
+if eAcuteQuestion == combinedEAcuteQuestion {
+    print("These two strings are considered equal")
+}
+
+
+// Conversely, LATIN CAPITAL LETTER A (U+0041, or "A"), as used in English, is not equivalent to CYRILLIC CAPITAL LETTER A (U+0410, or "А"), as used in Russian. The characters are visually similar, but do not have the same linguistic meaning
+let latinCapitalLetterA: Character = "\u{41}"
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+    print("These two characters are not equivalent.")
+}
+
+// Prefix and suffix equality
+
+// Method: asPrefix(_:)
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        act1SceneCount += 1
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+
+
+// Method: hasSuffix(_:)
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+
 // ======================= Unicode Representations of Strings ==========================
+
+let dogString = "Dog‼🐶"
+
+// UTF-8 representation
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("\n")
+
+// UTF-16 representation
+for codeUnit in dogString.utf16 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("\n")
+
+// Unicode scalar representation
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar.value) ", terminator: "")
+}
--- Playgrounds/TheSwiftProgrammingLanguage/Swift_4_CollectionTypes.playground/Contents.swift
+++ Playgrounds/TheSwiftProgrammingLanguage/Swift_4_CollectionTypes.playground/Contents.swift
@@ -6,15 +6,219 @@ import UIKit
 
 // ======================= Mutability of Collections ==========================
 
+let immutableArray = ["a", "b"]
+var mutableArray = ["c", "d"]
+//immutableArray.append("f")
+mutableArray.append("e")
+
 // ======================= Arrays ==========================
 
+// Creating an empty array
+var someInts = [Int]()
+print("someInts is of type [Int] with \(someInts.count)")
+
+someInts.append(3)
+someInts = []
+
+// Creating an array with a default value
+var threeDoubles = Array.init(repeating: 0.0, count: 3)
+
+// Creating an array by adding two arrays together
+var anotherThreeDoubles = Array.init(repeating: 2.5, count: 3)
+var sixDoubles = threeDoubles + anotherThreeDoubles
+
+// Creating an array with an array literal
+//var shoppingList: [String] = ["Egg", "Milk"]
+var shoppingList = ["Egg", "Milk"]
 
 
+// Assigning and modifying an array
+
+// Property: count
+print("The shopping list contains \(shoppingList.count) items")
+
+// Property: isEmpty
+if shoppingList.isEmpty {
+    print("The shooping list is empty")
+} else {
+    print("The shopping list is not empty")
+}
+
+// Method: append(_:)
+shoppingList.append("Flour")
+
+// append an array of one or more compatible items with the addition assignment operator (+=).
+shoppingList += ["Baking Powder"]
+
+//  subscript syntax
+var firstItem = shoppingList[0]
+shoppingList[0] = "six eggs"
+
+// You can also use subscript syntax to change a range of values at once, even if the replacement set of values has a different length than the range you are replacing.
+shoppingList[0...2] = ["Bananas", "Apples", "Oranges"]
+print(shoppingList)
+
+
+// Insert
+shoppingList.insert("Maple Syrup", at: 0)
+
+// Remove
+shoppingList.remove(at: 0)
+shoppingList.removeFirst()
+shoppingList.removeLast()
+
+
+// Iterating over an array
+for item in shoppingList {
+    print(item)
+}
+
+for(indx, value) in shoppingList.enumerated() {
+    print("Item (\(index) + 1): \(value)")
+}
 
 // ======================= Sets ==========================
 
+// Hash values for set types
+// A type must be hashable in order to be stored in a set—that is, the type must provide a way to compute a hash value for itself.
+
+// Creating and initializing an empty set 
+var letters = Set<Character>()
+print("letters is of type Set<Character> with \(letters.count) items.")
+
+letters.insert("a")
+letters = [] // letters is now an empty set, but is still of type Set<Character>
+
+// Creating a set with array literal
+var favoriteGenres: Set<String> = ["Rock", "Classical", "Hip hop"]
+// or
+//var favoriteGenres: Set = ["Rock", "Classical", "Hip hop"]
+
+
+// Accessing and modifying a set
+// Property :count
+print("I have \(favoriteGenres.count) favorite music genres.")
+
+// Property: isEmpty
+if favoriteGenres.isEmpty {
+    print("As far as music goes, I'm not picky.")
+}
+
+// Method: insert
+favoriteGenres.insert("Jazz")
+
+// Method: contains
+if favoriteGenres.contains("Jazz") {
+    print(" favoriteGenres contains Jazz")
+}
+
+// Remove
+if let removeGenre = favoriteGenres.remove("Rock") {
+    print("\(removeGenre)? I'm over it.")
+}
+
+// Iterating over a set
+
+for genre in favoriteGenres {
+    print("\(genre)")
+}
+
+for genre in favoriteGenres.sorted() {
+    print("\(genre)")
+}
 
 // ======================= Performing Set Operations ==========================
 
+// Fundamental set operations
+
+let oddDigits: Set = [1, 3, 5, 7, 9]
+let evenDigits: Set = [0, 2, 4, 6, 8]
+let singleDigitPrimeNumbers: Set = [2, 3, 5, 7]
+
+oddDigits.union(evenDigits).sorted()
+oddDigits.intersection(singleDigitPrimeNumbers).sorted()
+oddDigits.subtracting(singleDigitPrimeNumbers).sorted()
+oddDigits.symmetricDifference(singleDigitPrimeNumbers)
+
+// Set membership and equality
+let houseAnimals: Set = ["🐶", "🐈"]
+let farmAnimals: Set = ["🐂", "🐔", "🐑", "🐶", "🐈"]
+let cityAnimals: Set = ["🐦", "🐭"]
+
+houseAnimals.isSubset(of: farmAnimals)
+farmAnimals.isSuperset(of: houseAnimals)
+farmAnimals.isDisjoint(with: cityAnimals)
+
 // ======================= Dictionaries ==========================
+
+// Creating an empty dictionary
+
+var namesOfIntegers = [Int: String]()
+
+// If the context already provides type information, you can create an empty dictionary with an empty dictionary literal, which is written as [:].
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers = [:]
+
+// Creating a dictionary with a dictionary literal
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+// or
+// var airports = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
+
+// Accessing and modifying a dictionary
+
+// Property: count
+print("The airports dictionary contains \(airports.count) itms.")
+
+// Property: isEmpty
+if airports.isEmpty {
+    print("The airports dictionary is empty.")
+}
+
+// You can add a new item to a dictionary with subscript syntax.
+airports["LHR"] = "London"
+
+// You can also use subscript syntax to change the value associated with a particular key.
+airports["LHR"] = "London Heathrow"
+
+
+// Method: updateValue(_:forKey:)
+if let oldValue = airports.updateValue("Dublin Airport", forKey: "DUB") {
+    print("The old value for DUB was \(oldValue)")
+}
+
+// You can also use subscript syntax to retrieve a value from the dictionary for a particular key.
+
+if let airportName = airports["DUB"] {
+    print("The name of the airport is \(airportName).")
+}
+
+// You can use subscript syntax to remove a key-value pair from a dictionary by assigning a value of nil for that key.
+airports["APL"] = "Apple International"
+airports["APL"] = nil
+
+// Method: removeValue(forKey:)
+if let removedValue = airports.removeValue(forKey: "DUB") {
+    print("The removed airport's name is \(removedValue).")
+}
+
+
+
+// Iterating over a dictionary
+for (airportCode, airportName) in airports {
+    print("\(airportCode): \(airportName)")
+}
+
+// You can also retrieve an iterable collection of a dictionary’s keys or values by accessing its keys and values properties.
+for airportCode in airports.keys {
+    print("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    print("Airport name: \(airportName)")
+}
+
+// If you need to use a dictionary’s keys or values with an API that takes an Array instance, initialize a new array with the keys or values property.
+let airport = [String](airports.keys)
+let airportNames = [String](airports.values)
+
 
